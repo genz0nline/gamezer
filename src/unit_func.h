79d726d83fbd5d28e97428143c7da036f001523a
genzonline <genzonlinew@gmail.com>
2025-06-07 17:36:37 +0400

    cleaned up all the stuff for v2

--- src/unit_func.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNIT_FUNC_H
-#define UNIT_FUNC_H
-
-#include "environment.h"
-
-void update_unit_position(Level *lvl, Unit *unit, int milliseconds_passed);
-bool check_unit_collision(Level *lvl, Unit *unit, bool *landed);
-
-#endif // UNIT_FUNC_H
--- src/utils.c
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-#include <math.h>
-
-float distance(Coordinate a, Coordinate b) {
-	float square_dist = pow(a.x_m - b.x_m, 2) + pow(a.y_m - b.y_m, 2);
-
-	return pow(square_dist, .5);
-}
-
